2843debc14975716a8bffd6748e5c96ca1db9472
Sveeeen4545 <Sven@yard-it.nl>
2023-03-23 10:48:50 +0100

    Dialog shows up on collision

--- Assets/Sven/DialogNPC.cs
+++ Assets/Sven/DialogNPC.cs
@@ -12,24 +12,40 @@ public class DialogNPC : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Player"))
+        Debug.Log("Is colliding");
+        if (other.gameObject.CompareTag("Player"))
         {
-            if (Input.GetButtonDown("Check"))
-            {
+            Debug.Log("tag collision is working");
+            
+                //Debug.Log("Input is working");
                 branchingCanvas.SetActive(true);
                 dialogValue.value = myDialog;
-            }
+            
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("tag collision is off");
+
+            //Debug.Log("Input is off");
+            branchingCanvas.SetActive(false);
+            dialogValue.value = myDialog;
+
         }
     }
 }
