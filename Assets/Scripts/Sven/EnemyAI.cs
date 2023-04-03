5e4935f8da431143a4695d19a1a0b0ccd9cd6d9e
Sveeeen4545 <Sven@yard-it.nl>
2023-04-03 10:18:56 +0200

    as

--- Assets/Scripts/Sven/EnemyAI.cs
+++ Assets/Scripts/Sven/EnemyAI.cs
@@ -9,8 +9,11 @@ public class EnemyAI : MonoBehaviour
     private int waypointIndex;
     private Vector3 target;
     public List<Transform> waypoints2 = new List<Transform>();
+    public List<Transform> specialEventWaypoints = new List<Transform>();
     public Transform audioQueNewTarget;
-    public bool isTriggerdQue = true;
+    //public bool isTriggerdQue = true;
+    public bool specialEventTrigger = false;
+    public bool specialEventTrigger2 = true;
 
 
     // Start is called before the first frame update
@@ -27,36 +30,73 @@ public class EnemyAI : MonoBehaviour
         {
             IterateWaypointIndex();
             UpdateDestination();
-            if(waypointIndex == 3 && isTriggerdQue)
+            //if(waypointIndex == 3 && isTriggerdQue)
+            //{
+            //    //AddAudioQueWaypoint();
+            //    waypoints2.Add(audioQueNewTarget);
+            //    isTriggerdQue = false;
+            //}
+            //if(waypointIndex == 2)
+            //{
+            //    waypoints2.Remove(audioQueNewTarget);
+            //}
+            
+        }
+        if (specialEventTrigger)
+        {
+            if (specialEventTrigger2)
             {
-                //AddAudioQueWaypoint();
-                waypoints2.Add(audioQueNewTarget);
-                isTriggerdQue = false;
-            }
-            if(waypointIndex == 2)
-            {
-                waypoints2.Remove(audioQueNewTarget);
+                specialEventTrigger2 = false;
+                AddAudioQueWaypoint();
+                //AddAndRemove();
             }
         }
     }
 
     void UpdateDestination()
     {
-        target = waypoints2[waypointIndex].position;
+        if (specialEventTrigger)
+        {
+            Debug.Log(waypointIndex);
+            target = specialEventWaypoints[waypointIndex].position;
+        }
+        else
+        {
+            target = waypoints2[waypointIndex].position;
+        }
         agent.SetDestination(target);
     }
 
     void IterateWaypointIndex()
     {
         waypointIndex++;
-        if(waypointIndex == waypoints2.Count)
+        if (waypointIndex == waypoints2.Count)
+        {
+            waypointIndex = 0;
+        }
+        if (waypointIndex == specialEventWaypoints.Count)
         {
             waypointIndex = 0;
         }
     }
+
     void AddAudioQueWaypoint()
     {
-        waypoints2.Add(audioQueNewTarget);
+        specialEventWaypoints.Add(audioQueNewTarget);
+    }
+
+    IEnumerator AddAndRemove()
+    {
+        yield return null;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Waypoint"))
+        {
+            specialEventWaypoints.RemoveAt(0);
+        }
     }
 
 }
+//list length != 0
