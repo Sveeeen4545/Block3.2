622eb8fc465d64df6f5faf53ad319586caa13df5
Sveeeen4545 <Sven@yard-it.nl>
2023-04-05 09:30:21 +0200

    MainMenu

---
+++ Assets/Scripts/Sven/MenuScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public GameObject SettingsPanel;
+    public GameObject MenuPanel;
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void SettingsPanelEnable()
+    {
+        SettingsPanel.SetActive(true);
+        MenuPanel.SetActive(false);
+    }
+
+    public void SettingsPanelDisable()
+    {
+        SettingsPanel.SetActive(false);
+        MenuPanel.SetActive(true);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
