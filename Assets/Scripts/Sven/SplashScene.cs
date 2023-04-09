5836308051f0c3ffedd664978ae405750098d9a4
Sveeeen4545 <Sven@yard-it.nl>
2023-04-09 14:44:27 +0200

    Added Splashscreen
    Gang Gang, West Side, East Side

---
+++ Assets/Scripts/Sven/SplashScene.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScene : MonoBehaviour
+{
+    public string nextLevelName = "MainMenu";
+    public float waitTime = 3f;
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        SceneManager.LoadScene(nextLevelName);
+    }
+}
