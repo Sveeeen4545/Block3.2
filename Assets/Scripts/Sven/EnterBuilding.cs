e3d6dfb766f3b1b1d5177f286a0e22df906781a1
Sveeeen4545 <Sven@yard-it.nl>
2023-04-11 11:47:45 +0200

    scene transitions

--- Assets/Scripts/Sven/EnterBuilding.cs
+++ Assets/Scripts/Sven/EnterBuilding.cs
@@ -7,14 +7,27 @@ public class EnterBuilding : MonoBehaviour
 {
     public string buildingNameRoomEnter;
 
+    public Animator transition;
+
+    public float transitionTime = 1.5f;
+
     public void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            SceneManager.LoadScene(buildingNameRoomEnter);
+            StartCoroutine(LoadLevel());
         }
     }
 
+
+    IEnumerator LoadLevel()
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(buildingNameRoomEnter);
+    }
 
 
 
