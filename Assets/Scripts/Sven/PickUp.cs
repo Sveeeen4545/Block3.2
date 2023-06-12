d277d6bfcfcd487431b712f14875e0d0eb505b83
Sveeeen4545 <Sven@yard-it.nl>
2023-06-12 15:57:01 +0200

    awef

--- Assets/Scripts/Sven/PickUp.cs
+++ Assets/Scripts/Sven/PickUp.cs
@@ -6,13 +6,23 @@ public class PickUp : MonoBehaviour
 {
     public GameObject ItemToDespawn;
     public GameObject ItemToSpawn;
+    public GameObject ItemPickedUpNoti;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            //StartCoroutine(ItemOnAndOff());
             ItemToDespawn.SetActive(false);
             ItemToSpawn.SetActive(true);
         }
     }
+    /*
+    IEnumerator ItemOnAndOff()
+    {
+        ItemPickedUpNoti.SetActive(true);
+        yield return new WaitForSeconds(4f);
+        ItemPickedUpNoti.SetActive(false);
+    }
+    */
 }
